39e969385abdc8acb86770242de06e3245994f1c
Anton Lugom <mogul086@mail.ru>
2023-01-26 22:31:50 +0300

    Начальная разметка "однооконного" приложения

---
+++ M3Practice13/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using M3Practice13.ViewModels.Base;
+using M3Practice13.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace M3Practice13.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        UserControl curentView;
+
+        public UserControl CurentView
+        {
+            get => curentView;
+            set => Set(ref curentView, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            CurentView = new RoleChoosingViewModel();
+        }
+
+    }
+}
