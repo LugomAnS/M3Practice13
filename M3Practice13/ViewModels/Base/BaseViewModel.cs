b645211edf7e2fc8760e319ab6d7be6abf06ec3c
Anton Lugom <mogul086@mail.ru>
2023-01-26 20:02:41 +0300

    Базовая инфраструктура

---
+++ M3Practice13/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3Practice13.ViewModels.Base
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+
+        public bool Set<T>(ref T field, T value, [CallerMemberName]string property = null)
+        {
+            if (Equals(field, value)) return false;
+
+            field = value;
+            OnPropertyChanged(property);
+            return true;
+        }
+    }
+}
---
+++ M3Practice13/ViewModels/Base/Command.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace M3Practice13.ViewModels.Base
+{
+    internal class Command : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public Command(Action<object> Execute, Predicate<object> CanExecute = null)
+        {
+            execute = Execute;
+            canExecute = CanExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter)
+            => canExecute?.Invoke(parameter) ?? true;
+
+
+        public void Execute(object? parameter)
+            => execute?.Invoke(parameter);
+    }
+}
