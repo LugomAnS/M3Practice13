12db7df2e0ac4ba9cb40e8e0511183ecd3e3b5d0
Anton Lugom <mogul086@mail.ru>
2023-02-03 19:44:49 +0300

    Исключение

---
+++ DataProvider/Models/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3Practice13.Models
+{
+    public class Manager : Worker
+    {
+        public override string ClientPassport
+        {
+            get => SelectedClientInfo.Client.Passport;
+        }
+    }
+}
